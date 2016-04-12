d9d7ac6f00ddabef10d53472a9a6670b0a8cd4ff
gevgeny <e.gluhotorenko@gmail.com>
2016-04-12 16:05:37 +0300

    restore typings for Highcharts

--- dist/Highcharts.d.ts
+++ dist/Highcharts.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="../typings/browser/ambient/highcharts/index.d.ts" />
+/// <reference path="../typings/browser/ambient/highstock/index.d.ts" />
 declare var Highcharts: any;
 export { Highcharts };
